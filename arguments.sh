1a6e50435ce3b3268bce47648918f73777b63666
SCL <76017102+SimClb@users.noreply.github.com>
2022-11-09 11:11:49 +0100

    exo about arguments

--- arguments.sh
+++ arguments.sh
@@ -1,8 +1,30 @@
-#!/bin/bash
-echo "File name is "$0 # holds the current script
-echo $2
-echo $3 # $3 holds banana
-echo $4
-Data=$5
-echo "A $Data costs just $6."
-echo $#
+#! /bin/zsh 
+
+# You can define your arguments when you are exe your scripte !
+
+Date=$1 
+echo $1
+echo $# 
+# contain how much there were arguments, bon jpasse en fr mon english is so pourri... Du coup $# contient le nombre d'arguemnts 
+#transmis au script
+echo $@ 
+# take all of arguments and write them next to each other, feel free to try ;)
+# % zsh arguments.sh 5
+# >>> 5
+
+echo $*
+# same of $@
+
+function File {
+    # think you are inside the file
+    # Change here
+    echo $#
+}
+
+# Do not change anything
+if [ ! $# -lt 1 ]; then
+    File $*
+    exit 0
+fi
+
+
