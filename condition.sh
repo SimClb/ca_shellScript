4ca4a8b1c75dc9b173d6c4dbe7f7a3fbe20ea4fd
SCL <76017102+SimClb@users.noreply.github.com>
2022-11-09 15:44:00 +0100

    exo about condition finished

--- condition.sh
+++ condition.sh
@@ -30,3 +30,35 @@ case "$request" in
     ;;
     *) echo "Press a correct  key, thanks."
 esac
+
+# a simple case bash structure 
+
+mycase=4
+case $mycase in
+    1) echo "You selected bash";;
+    2) echo "You selected perl";;
+    3) echo "You selected phyton";;
+    4) echo "You selected c++";;
+    5) exit
+esac
+
+#----------------------- Exo ---------------------------
+
+# change these variables
+NUMBER=16
+APPLES=16
+KING="LUIS"
+# modify above variables
+# to make all decisions below TRUE
+if [ $NUMBER -gt 15 ] ; then
+  echo 1
+fi
+if [ $NUMBER -eq $APPLES ] ; then
+  echo 2
+fi
+if [[ ($APPLES -eq 12) || ("$KING" = "LUIS") ]] ; then
+  echo 3
+fi
+if [[ $(($NUMBER + $APPLES)) -le 32 ]] ; then
+  echo 4
+fi
